fd627124dc43eac16c738cac8e54d140d04e58a6
Dan Lindsay <dlindsay72@hotmail.com>
2018-02-27 15:40:22 -0500

    basic organization, some UI and outlets, actions for RegisterVC

---
+++ Tweeter/Controller/RegisterVC.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterVC.swift
+//  Tweeter
+//
+//  Created by Dan Lindsay on 2018-02-27.
+//  Copyright © 2018 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+    
+    
+    @IBOutlet weak var usernameTxtField: UITextField!
+    @IBOutlet weak var emailTxtField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    @IBOutlet weak var firstNameTxtField: UITextField!
+    @IBOutlet weak var lastNameTxtField: UITextField!
+    @IBOutlet weak var registerBtn: UIButton!
+    @IBOutlet weak var toLoginScreenBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func registerBtnTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func toLoginScreenBtnTapped(_ sender: Any) {
+        
+    }
+    
+
+}
+
