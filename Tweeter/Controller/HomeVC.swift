5b6fcc88d4303cecb0efc67daf532623e87cb04a
Dan Lindsay <dlindsay72@hotmail.com>
2018-04-11 12:28:05 -0400

    can now edit profile pic and the image is saved in our database. When app loads and user signs in, profile pic previously selected will show in avatarImage on HomeVC

--- Tweeter/Controller/HomeVC.swift
+++ Tweeter/Controller/HomeVC.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
+public var parseJSONKey = "parseJSON"
+public let localHostKey = "http://localhost:8080/TweeterBackend/uploadAva.php"
+
 class HomeVC: UIViewController {
+    
+    //MARK: - IBOutlets
 
     @IBOutlet weak var avatarImage: UIImageView!
     @IBOutlet weak var usernameLbl: UILabel!
@@ -16,12 +21,19 @@ class HomeVC: UIViewController {
     @IBOutlet weak var emailLbl: UILabel!
     @IBOutlet weak var editProfileBtn: UIButton!
     
+    //MARK: - Class Properties
+    
+    let picker = UIImagePickerController()
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return UIStatusBarStyle.lightContent
     }
     
+    //MARK: - Class Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
         
         let username = (user!["username"] as AnyObject).uppercased
         let fullname = user!["fullname"] as? String
@@ -38,15 +50,120 @@ class HomeVC: UIViewController {
         // get user profile pic
         
         if ava != "" {
-            
+            // url path to image
+            let imageURL = URL(string: ava!)!
+
+            // communicate back user as main queue
+            DispatchQueue.main.async(execute: {
+
+                // get data from image url
+                let imageData = try? Data(contentsOf: imageURL)
+
+                // if data is not nill assign it to ava.Img
+                if imageData != nil {
+                    DispatchQueue.main.async(execute: {
+                        self.avatarImage.image = UIImage(data: imageData!)
+                    })
+                }
+            })
         }
-        
 
     }
     
+    
+    
+    //MARK: - Custom Class Methods
+    @objc func createBodyWith(parameters: [String: String]?, filePathKey: String?, imageDataKey: Data, boundary: String) -> Data {
+        let body = NSMutableData()
+        
+        if parameters != nil {
+            for (key, value) in parameters! {
+                body.appendString("--\(boundary)\r\n")
+                body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                body.appendString("\(value)\r\n")
+            }
+        }
+        
+        let filename = "ava.jpg"
+        let mimeType = "image/jpg"
+        
+        body.appendString("--\(boundary)\r\n")
+        body.appendString("Content-Disposition: form-data; name=\"\(filePathKey!)\"; filename=\"\(filename)\"\r\n")
+        body.appendString("Content-Type: \(mimeType)\r\n\r\n")
+        body.append(imageDataKey)
+        body.appendString("\r\n")
+        body.appendString("--\(boundary)--\r\n")
+        
+        return body as Data
+    }
+    
+    @objc func uploadAva() {
+
+        let id = user!["id"] as! String
+        let url = URL(string: localHostKey)!
+        var request = URLRequest(url: url)
+
+        request.httpMethod = "POST"
+
+        let param = ["id": id]
+        let boundary = "Boundary-\(UUID().uuidString)"
+
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        let imageData = UIImageJPEGRepresentation(avatarImage.image!, 0.5)
+
+        if imageData == nil {
+            return
+        }
+
+        request.httpBody = createBodyWith(parameters: param, filePathKey: "file", imageDataKey: imageData!, boundary: boundary)
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            DispatchQueue.main.async(execute: {
+                if error == nil {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+
+                        guard let parseJSON = json else {
+                            print("Error while parsing: \(error.debugDescription)")
+                            return
+                        }
+                        print(parseJSON)
+                        let id = parseJSON["id"]
+
+                        if id != nil {
+                            UserDefaults.standard.set(parseJSON, forKey: parseJSONKey)
+                            user = UserDefaults.standard.value(forKey: parseJSONKey) as? NSDictionary
+                        } else {
+                            DispatchQueue.main.async(execute: {
+                                let message = parseJSON["message"] as! String
+                                appDelegate.showInfoView(message: message, color: customOrange)
+                            })
+                        }
+                    } catch {
+                        DispatchQueue.main.async(execute: {
+                            let message = error as! String
+                            appDelegate.showInfoView(message: message, color: customOrange)
+                        })
+                    }
+                } else {
+                    DispatchQueue.main.async(execute: {
+                        let message = error!.localizedDescription
+                        appDelegate.showInfoView(message: message, color: customOrange)
+                    })
+                }
+            })
+        }.resume()
+    }
+    
+    
+    
+    
+    // MARK: - IBActions
+    
     @IBAction func editProfileBtnWasPressed(_ sender: Any) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
+        
+        
         picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
         picker.allowsEditing = true
         
@@ -56,10 +173,22 @@ class HomeVC: UIViewController {
 
 }
 
+//MARK: UINavigationControllerDelegate, UIImagePickerControllerDelegate
+
 extension HomeVC: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         avatarImage.image = info[UIImagePickerControllerEditedImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
+        uploadAva()
+    }
+}
+
+// MARK: - NSMutableData
+
+extension NSMutableData {
+    func appendString(_ string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
     }
 }
 
